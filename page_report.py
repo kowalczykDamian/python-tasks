9dcfa2fdb4add4f219a48d7371d36ed4497b12b9
kowalczykDamian <36140669+kowalczykDamian@users.noreply.github.com>
2018-02-12 13:29:31 +0100

    Update page_report.py

--- page_report.py
+++ page_report.py
@@ -2,24 +2,25 @@ import sys
 from urllib.parse import urlparse
 from collections import Counter
 import operator
+import re
 
+pattern = re.compile(r'\d{2}\.\d\.\d{3}\.\d{3}\s[[]\d{2}[/][A-Z][a-z]{2}[/]\d{4}[:]\d{2}[:]\d{2}[:]\d{2}\s[+]\d{4}[]]\s["][A-Z]{3}\s(https?://[a-z./?=&]+)\s[A-Z]{4}[/]\d\.\d["]\s\d{3}\s\d{4}')
 logs_list = []
-
-# open file using context manager
-with open(sys.argv[1], 'r') as file:    
-    for line in file:
-        logs_list.append(line)
-
 urls_list = []
 
+# read file using context manager
+with open(sys.argv[1], 'r') as file:
+    for num, line in enumerate(file, 1):
+        if pattern.match(line):
+            logs_list.append(line)
+        else:
+            sys.stderr.write("Invalid log lines: " + str(num) + '\n')
+
 # parse url from log
-for i in range(0, len(logs_list)):
+for log in logs_list:
 
-    split_log = logs_list[i].split('"')
-    whole_http_request = split_log[1]
-
-    split_request = whole_http_request.split(' ')
-    whole_url = split_request[1]
+    # get first group from regex pattern
+    whole_url = pattern.sub(r'\1', log).rstrip('\n')        
 
     parser = urlparse(whole_url)
     url = parser.netloc + parser.path
@@ -32,4 +33,4 @@ num_of_url = Counter(urls_list)
 
 # print sorted output
 for k, v in sorted(num_of_url.items(), key=operator.itemgetter(1), reverse=True):
-    print("\""+k+"\","+str(v))
+    print("\"" + k + "\"," + str(v))
